tritonka/logstash

Expose pipeline metrics to /_node/stats/pipelines

logstash-core/lib/logstash/api/modules/node_stats.rb
@@ -40,6 +40,10 @@ module LogStash
           respond_with :process => process_payload
         end
 
+        get "/pipelines" do
+          respond_with :pipelines => pipeline_payload
+        end
+
         private
 
         def events_payload
@@ -52,6 +56,10 @@ module LogStash
 
         def process_payload
           @stats.process
+        end
+
+        def pipeline_payload
+          @stats.pipeline
         end
       end
     end

kingsqv/logstash-1

logstash-core/lib/logstash/api/modules/node_stats.rb
@@ -50,6 +50,10 @@ module LogStash
           respond_with :mem => mem_payload
         end
 
+        get "/pipeline" do
+          respond_with :pipeline => pipeline_payload
+        end
+
         private
 
         def events_payload
@@ -66,6 +70,10 @@ module LogStash
 
         def mem_payload
           @stats.memory
+        end
+
+        def pipeline_payload
+          @stats.pipeline
         end
       end
     end